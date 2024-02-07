af70fba7236cbdaa53ab125142bea420a5ed22b5
Ratan Tejaswi <ratantejaswi@gmail.com>
2024-02-07 16:56:34 -0500

    Initial Commit

---
+++ SweetSpot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SweetSpot
+//
+//  Created by Ratan Tejaswi Vadapalli on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SweetSpot/SweetSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSpotApp.swift
+//  SweetSpot
+//
+//  Created by Ratan Tejaswi Vadapalli on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SweetSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
